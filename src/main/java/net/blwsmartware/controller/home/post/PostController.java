d5ee2fbc9f18807cf26c1ab51421ca55f36cafef
Tcrow06 <Tcrow0604@gmail.com>
2024-08-08 22:50:02 +0700

    them post-image-api, method delete-post

--- src/main/java/net/blwsmartware/controller/home/post/PostController.java
+++ src/main/java/net/blwsmartware/controller/home/post/PostController.java
@@ -101,6 +101,7 @@ import java.util.stream.Collectors;
 
             PostModel postModel = new ObjectMapper().convertValue(formData, PostModel.class);
 
+
             String tagList = request.getParameter("tag-collect");
             Pattern pattern = Pattern.compile("#\\w+");
             Matcher matcher = pattern.matcher(tagList);
@@ -112,6 +113,7 @@ import java.util.stream.Collectors;
             if(!tags.isEmpty()){
                 postModel.setTab(tags);
             }
+
             if (rez != null) {
                 postModel.setImagePath(rez.getPath());
                 postModel.setThumbnail(imageService.getThumbnailName());
@@ -164,6 +166,14 @@ import java.util.stream.Collectors;
             }
 
             PostModel postModel = new ObjectMapper().convertValue(formData, PostModel.class);
+            postModel = postService.findByID(postModel.getId());
+            if(postModel.getImagePath()!=null && !postModel.getImagePath().isEmpty()){
+                File oldImage = new File(postModel.getImagePath());
+                boolean deleted = oldImage.delete();
+                if(!deleted){
+                    System.out.println("Delete image unsuccessfully");
+                }
+            }
 
             String tagList = request.getParameter("tag-collect");
             Pattern pattern = Pattern.compile("#\\w+");
